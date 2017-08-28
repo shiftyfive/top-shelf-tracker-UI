c68a7d6f1aa3a7aae5c48fc8550ce87ef0615505
Joshua <joshuawarren000@gmail.coml>
2017-08-28 04:53:35 -0700

    finished styling on event componenet ... for now

--- src/components/event.jsx
+++ src/components/event.jsx
@@ -3,22 +3,36 @@ import superagent from 'superagent';
 import style from '../styles/event.css';
 import Button from 'material-ui/RaisedButton';
 
+const styles = {
+  button: {
+    borderRaius: 40,
+  }
+}
 
 const Event = props => (
-  <div className="event">
-    <h4>{props.playerName}</h4>
-    <div className="col1">
-      <h5>Event Type: {props.eventType}</h5>
-      <h5>Zone: {props.eventZone}</h5>
+  <div className="event-container">
+    <div className="event-row">
+      <h4 className="left">Player: {props.playerName}</h4>
+      <h4 className="right"> #27 {props.jersey_number}</h4>
     </div>
-    <div className="col2">
-      <h5>Time: {props.eventTime}</h5>
-      <h5>result: {props.result}</h5>
-      <Button
-        variant="raised"
-        label="Edit"
-        default
-      />
+    <div className="event-row">
+      <h5 className="left">Event Type: <span className="event-text">{props.eventType}</span></h5>
+      <h5 className="right">result: <span className="event-text">{props.result}</span></h5>
+    </div>
+    <div>
+      <div className="event-row">
+        <h5 className="left">Zone: <span className="event-text">{props.eventZone}</span></h5>
+        <h5 className="right">Time: <span className="event-text">{props.eventTime}</span></h5>
+      </div>
+      <div>
+        <Button
+          className="button-edit-event"
+          primary={true}
+          variant="raised"
+          label="Edit"
+          default
+        />
+      </div>
     </div>
   </div>
 );
