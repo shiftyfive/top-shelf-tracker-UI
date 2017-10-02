8d2a0e31f4702a86079ce66984664f9be5ee8c85
Joshua <joshuawarren000@gmail.coml>
2017-10-02 03:37:33 -0400

    added https back

--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
-const AUTH_URL = `http://top-shelf-tracker-api.herokuapp.com/`;
+const AUTH_URL = `https://top-shelf-tracker-api.herokuapp.com/`;
 
 export default AUTH_URL;
