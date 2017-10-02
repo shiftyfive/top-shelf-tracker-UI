cdd7817c2458847dac086f9620cee367006aa42d
Joshua <joshuawarren000@gmail.coml>
2017-10-02 03:19:17 -0400

    added auth url back

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-
-const AUTH_URL = `https://top-shelf-tracker-api.herokuapp.com/`;
+const AUTH_URL = window.location.host.indexOf('localhost') > -1 ? 'http://localhost:3000/' : null;
 
 export default AUTH_URL;
